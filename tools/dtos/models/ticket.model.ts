2e22432f4ddbbe909df9a61e6f4950f3e66f25ae
Batikan Ugur Tizer <batikan.tizer@colendi.com>
2020-11-15 17:17:35 +0300

    models and dtos added

---
+++ tools/dtos/models/ticket.model.ts
@@ -0,0 +1,15 @@
+import { ActivityModel } from "./activity.model";
+import { AuditModel } from "./audit.model";
+import { TicketTypeModel } from "./ticket-type.model";
+import { UserModel } from "./user.model";
+
+export class TicketModel {
+    id: string;
+    name: string;
+    description: string;
+    type: TicketTypeModel;
+    responsible: UserModel;
+    audit: AuditModel;
+    activities: ActivityModel[];
+    inventories: InventoryModel[];
+}
